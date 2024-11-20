4d98ad4bc208bf6c35de97c3a34a26b963fc9ea4
Maximo Patron <108689781+MaxiPatron@users.noreply.github.com>
2024-11-20 17:19:53 -0300

    Portfolio

---
+++ src/routes/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+import BackImg from '../components/BackImg'
+import Footer from '../components/Footer'
+import About from '../components/About'
+import Skills from '../components/Skills'
+const Home = () => {
+  return (
+    <div>
+      <NavBar />
+      <BackImg />
+      <About />
+      <Skills />
+      <Footer />
+    </div>
+  )
+}
+export default Home
